89c82e33976302c7ec1fb2ad7938a4ffb0f602a7
Aman Yadav <amanyddv@gmail.com>
2023-05-04 17:53:01 +0530

    done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { AboutComponent } from './pages/about/about.component';
-import { ServiceComponent } from './pages/service/service.component';
-import { ContactComponent } from './pages/contact/contact.component';
+import { AboutComponent } from './pages/about us/about.component';
+import { ServiceComponent } from './pages/our service/service.component';
+import { ContactComponent } from './pages/contact us/contact.component';
 import { FaqsComponent } from './pages/faqs/faqs.component';
 const routes: Routes = [
   { path: '', component: HomeComponent},
