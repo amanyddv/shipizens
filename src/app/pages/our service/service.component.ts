9c789d72c36ce8f7d035ef512f0bd879cf516e47
Aman Yadav <amanyddv@gmail.com>
2023-05-05 17:41:45 +0530

    our service responsive

--- src/app/pages/our service/service.component.ts
+++ src/app/pages/our service/service.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-service',
   templateUrl: './service.component.html',
-  styleUrls: ['./service.component.css','./style.css','./responsive.css']
+  styleUrls: ['./service.component.css','./responsive.css']
 })
 export class ServiceComponent {
 
